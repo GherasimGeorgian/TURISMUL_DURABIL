a34483bfa0681f3c31ca385b473d85b21d640319
Geo <Geo@DESKTOP-4HDFJN5>
2020-08-26 21:16:54 +0300

    Genereaza excursie/Vizualizare itinerariu/Vizualizare Imagini

--- TURISMUL_DURABIL/Form3.Designer.cs
+++ TURISMUL_DURABIL/Form3.Designer.cs
@@ -29,11 +29,269 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(609, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 47);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Genereaza excursie";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(308, 38);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 13;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "dd.MM.yyyy";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(61, 38);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(330, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Finalizare excursie";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Inceput excursie";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(120, 104);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(653, 375);
+            this.tabControl1.TabIndex = 15;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(645, 349);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Planificari";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(639, 343);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(645, 349);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Perioade de vizitare";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView2.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(639, 343);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.dataGridView3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(645, 349);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Vizualizare itinerariu";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView3.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(639, 343);
+            this.dataGridView3.TabIndex = 0;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.progressBar1);
+            this.tabPage4.Controls.Add(this.pictureBox1);
+            this.tabPage4.Controls.Add(this.label4);
+            this.tabPage4.Controls.Add(this.label3);
+            this.tabPage4.Controls.Add(this.button2);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(645, 349);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Vizualizare imagini";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(404, 48);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(201, 36);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(15, 48);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(323, 212);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(244, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "label4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "label3";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Wheat;
+            this.button2.Font = new System.Drawing.Font("Monotype Corsiva", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Location = new System.Drawing.Point(451, 124);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 48);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(957, 530);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form3";
             this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- TURISMUL_DURABIL/Form3.cs
+++ TURISMUL_DURABIL/Form3.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +18,262 @@ namespace TURISMUL_DURABIL
         public Form3()
         {
             InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection(Form1.constr);
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            LoadPlanificari();
+        }
+        private void filtrare()
+        {
+            BindingSource bs = new BindingSource();
+
+            bs.DataSource = dataGridView2.DataSource;
+            bs.Filter = "DataStart >= '" + dateTimePicker1.Value.AddDays(-1) + "' And " + "DataStop <= '" + dateTimePicker2.Value.AddDays(1) + "'";
+            dataGridView2.DataSource = bs;
+        }
+        private void filtrare2()
+        {
+            BindingSource bs = new BindingSource();
+
+            bs.DataSource = dataGridView2.DataSource;
+            bs.Filter = "Nume like 'B%'";
+            dataGridView2.DataSource = bs;
+        }
+        private void LoadPlanificari()
+        {
+            con.Open();
+
+            DataTable table = new DataTable();
+
+            SqlCommand cmd = new SqlCommand("select l.Nume,p.DataStart,p.DataStop,p.Frecventa,p.Ziua FROM Localitati l,Planificari p WHERE p.IDlocalitate = l.IDLocalitate", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+            da.Fill(table);
+
+            dataGridView1.DataSource = table;
+            dataGridView1.Columns[0].Name = "Nume";
+            dataGridView1.Columns[1].Name = "DataStart";
+            dataGridView1.Columns[2].Name = "DataStop";
+            dataGridView1.Columns[3].Name = "Frecventa";
+            dataGridView1.Columns[4].Name = "Ziua";
+            con.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            adaugarevalori();
+          
+
+            datagrid3();
+        }
+        private void datagrid3()
+        {
+            int ok = 0;
+            DataTable table3 = new DataTable();
+            table3.Columns.Add("Nume", typeof(string));
+            table3.Columns.Add("Data", typeof(DateTime));
+            String searchValue2 = "lunar";
+            String searchValue3 = "anual";
+            String searchValue1 = "ocazional";
+            int rowIndex = 0;
+            foreach (DataGridViewRow row in dataGridView2.Rows)
+            {
+
+                if (dataGridView2.Rows[rowIndex].Cells[3].Value.ToString().Equals(searchValue1))
+                {
+
+                    string nume = dataGridView2.Rows[rowIndex].Cells[0].Value.ToString();
+
+                    DateTime datastart = Convert.ToDateTime(dataGridView2.Rows[rowIndex].Cells[1].Value.ToString());
+                    DateTime datastop = Convert.ToDateTime(dataGridView2.Rows[rowIndex].Cells[2].Value.ToString());
+                    int dsdays = datastart.Day;
+                    int dtdays = datastop.Day;
+                    int dif = dtdays - dsdays;
+                    if (ok == 1)
+                    {
+                        for (int i = 0; i <= dif; i++)
+                         {
+                         table3.Rows.Add(nume, datastart.AddDays(i));
+                         }
+                    }
+                    else ok++;
+
+                }
+                if (dataGridView2.Rows[rowIndex].Cells[3].Value.ToString().Equals(searchValue2))
+                {
+                    string nume = dataGridView2.Rows[rowIndex].Cells[0].Value.ToString();
+                    DateTime data = Convert.ToDateTime(dataGridView2.Rows[rowIndex].Cells[1].Value.ToString());
+                    table3.Rows.Add(nume, data);
+                }
+                if (dataGridView2.Rows[rowIndex].Cells[3].Value.ToString().Equals(searchValue3))
+                {
+                    string nume = dataGridView2.Rows[rowIndex].Cells[0].Value.ToString();
+                    DateTime data = Convert.ToDateTime(dataGridView2.Rows[rowIndex].Cells[1].Value.ToString());
+                    table3.Rows.Add(nume, data);
+                }
+
+
+
+                rowIndex = row.Index;
+
+            }
+            table3.DefaultView.Sort = "Data asc";
+            table3 = table3.DefaultView.ToTable();
+            dataGridView3.DataSource = table3;
+        }
+        private void adaugarevalori()
+        {
+            DataTable table2 = new DataTable();
+
+            table2.Columns.Add("Nume", typeof(string));
+            table2.Columns.Add("DataStart", typeof(DateTime));
+            table2.Columns.Add("DataStop", typeof(DateTime));
+            table2.Columns.Add("Frecventa", typeof(string));
+            String searchValue = "ocazional";
+            String searchValue1 = "anual";
+            String searchValue2 = "lunar";
+            int rowIndex = 0;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+
+                if (dataGridView1.Rows[rowIndex].Cells[3].Value.ToString().Equals(searchValue))
+                {
+                    string nume = dataGridView1.Rows[rowIndex].Cells[0].Value.ToString();
+
+                    DateTime datastart = Convert.ToDateTime(dataGridView1.Rows[rowIndex].Cells[1].Value.ToString());
+                    DateTime datastop = Convert.ToDateTime(dataGridView1.Rows[rowIndex].Cells[2].Value.ToString());
+
+
+                    //cazul datei intre cele doua picktime
+                    if (datastart >= dateTimePicker1.Value.Date && datastop <= dateTimePicker2.Value.Date) {
+                        table2.Rows.Add(nume, datastart, datastop, searchValue);
+                       
+                    }
+                   
+                    // cazul in care datastart este mai mare si data stop mai mare
+                    if (datastart >= dateTimePicker1.Value.Date && datastop >= dateTimePicker2.Value.Date && datastart <= dateTimePicker2.Value.Date)
+                    {
+                        datastop = dateTimePicker2.Value.Date;
+                        if(datastop > datastart)
+                        table2.Rows.Add(nume, datastart, datastop, searchValue);
+                    }
+                    //cazul in care datastart este mai mic si datastop este mai mic
+                    if (datastart <= dateTimePicker1.Value.Date && datastop <= dateTimePicker2.Value.Date && datastart <= dateTimePicker2.Value.Date)
+                    {
+                        datastart = dateTimePicker1.Value.Date;
+                        if (datastop > datastart)
+                            table2.Rows.Add(nume, datastart, datastop, searchValue);
+                    }
+
+
+
+                }
+                if (dataGridView1.Rows[rowIndex].Cells[3].Value.ToString().Equals(searchValue1))
+                {
+
+                    int an_d1 = dateTimePicker1.Value.Year;
+                    int an_d2 = dateTimePicker2.Value.Year;
+                    int dif = an_d2 - an_d1;
+                    string nume = dataGridView1.Rows[rowIndex].Cells[0].Value.ToString();
+                    int doy = Int32.Parse(dataGridView1.Rows[rowIndex].Cells[4].Value.ToString()) - 1;
+
+                    DateTime value = new DateTime(an_d1, 1, 1);
+                    
+                    for (int i = 0; i <= dif; i++)
+                    {
+                        DateTime convertedDate = value.AddYears(i).AddDays(doy);
+                        
+                        if(convertedDate >= dateTimePicker1.Value.Date && convertedDate <= dateTimePicker2.Value.Date)
+                        table2.Rows.Add(nume, convertedDate, convertedDate, searchValue1);
+
+                    }
+                }
+                if (dataGridView1.Rows[rowIndex].Cells[3].Value.ToString().Equals(searchValue2))
+                {
+
+                    int an_d1 = dateTimePicker1.Value.Year;
+                    int an_d2 = dateTimePicker2.Value.Year;
+                    int dif = an_d2 - an_d1;
+                    string nume = dataGridView1.Rows[rowIndex].Cells[0].Value.ToString();
+                    int luna = Int32.Parse(dataGridView1.Rows[rowIndex].Cells[4].Value.ToString()) - 1;
+
+                    DateTime value = new DateTime(an_d1, 1, 1);
+                    
+
+                    for (int i = 0; i <= dif; i++)
+                    {
+                        for (int j = 0; j < 12; j++)
+                        {
+                            DateTime xxx = value.AddYears(i).AddMonths(j);
+                            int days = DateTime.DaysInMonth(xxx.Year, xxx.Month);
+
+                            if (days >= luna) {
+                                DateTime convertedDate = value.AddYears(i).AddMonths(j).AddDays(luna);
+                                if (convertedDate >= dateTimePicker1.Value.Date && convertedDate <= dateTimePicker2.Value.Date)
+                                    table2.Rows.Add(nume, convertedDate, convertedDate, searchValue2);
+                            }
+                        }
+                    }
+
+                }
+                rowIndex = row.Index;
+            }
+            dataGridView2.DataSource = table2;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView2.Rows.Count > 0)
+            { timer1.Start(); }
+            else
+            {
+                MessageBox.Show("Genereaza o excursie!");
+            }
+        }
+        int x = 0;
+        int rowIndex = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (rowIndex >= dataGridView3.Rows.Count - 1)
+            {
+                rowIndex = 0;
+            }
+
+            string nume = dataGridView3.Rows[rowIndex].Cells[0].Value.ToString();
+
+            string[] files = Directory.GetFiles(Application.StartupPath + @"\..\..\Imagini\", "*" + nume + "*.*");
+
+            if (x >= files.Length)
+            {
+                x = 0;
+            }
+
+            pictureBox1.Image = Image.FromFile(files[x]);
+
+            DateTime datapickstart = Convert.ToDateTime(dateTimePicker1.Value.ToString());
+            int n = Convert.ToInt32(datapickstart.ToString("yyyyMMdd"));
+            DateTime datapickstop = Convert.ToDateTime(dateTimePicker2.Value.ToString());
+            int m = Convert.ToInt32(datapickstop.ToString("yyyyMMdd"));
+
+            progressBar1.Minimum = n;
+            progressBar1.Maximum = m;
+
+
+
+            DateTime data = Convert.ToDateTime(dataGridView3.Rows[rowIndex].Cells[1].Value.ToString());
+            string startdate = data.ToString("dd/MM/yyyy");
+            int y = Convert.ToInt32(data.ToString("yyyyMMdd"));
+
+
+
+            label3.Text = nume.ToString();
+            label4.Text = startdate.ToString();
+            progressBar1.Value = y;
+
+
+
+            rowIndex++; x++;
         }
     }
 }
